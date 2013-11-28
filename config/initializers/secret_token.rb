cf039cef1fc313a8e31840d461515b74166d43b4
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-27 19:46:41 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrainTrade::Application.config.secret_key_base = '6a4e2ea3a8fab53de6a079e79c8232788ea5ff3d3f17c5d1d9bd5256478f12e0413a5fe34e72c95fa29f6168d57815181a03fa14e41062ea6913ceecf09f4cfe'
