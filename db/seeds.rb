acefd6f4f1a3138c15207bf51bc88055614c43cf
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-29 17:10:17 -0600

    seed file, cat browsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+art = Category.create(name: 'Art')
+cooking = Category.create(name: 'Cooking')
+crafts = Category.create(name: 'Crafts')
+economics = Category.create(name: 'Economics')
+history = Category.create(name: 'History')
+languages = Category.create(name: 'Languages')
+life_sciences = Category.create(name: 'Life Sciences')
+literature = Category.create(name: 'Literature')
+miscellaneous = Category.create(name: 'Miscellaneous')
+math = Category.create(name: 'Mathematics')
+phys_sciences = Category.create(name: 'Physical Sciences')
+programming = Category.create(name: 'Programming')
+social_sciences = Category.create(name: 'Social Sciences')
+writing = Category.create(name: 'Writing')
+
+
+french = languages.subjects.create(name: 'French')
+spanish = languages.subjects.create(name: 'Spanish')
+german = languages.subjects.create(name: 'German')
+asl = languages.subjects.create(name: 'ASL')
+norwegian = languages.subjects.create(name: 'Norwegian')
+portuguese = languages.subjects.create(name: 'Portuguese')
+
+meara = User.create(first_name: 'Meara', last_name: 'Charnetzki', email: 'meara@dbc.com', password_digest: 1)
+peter = User.create(first_name: 'Peter', last_name: 'Pan', email: 'pete@pan.com', password_digest: 1)
+
+meara_french = french.offerings.create(teacher_id: 1, hangout: true)
+peter_french = french.offerings.create(teacher_id: 2, hangout: true)
+peter_asl = asl.offerings.create(teacher_id: 2, hangout: true)
