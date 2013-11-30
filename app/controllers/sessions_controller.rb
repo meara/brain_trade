3ca8a81309a60911812104a4d9054f91be5fdaaa
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-29 18:18:47 -0600

    Added methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,27 @@
 class SessionsController < ApplicationController
 
   def index
+    #HOMEPAGE
+  end
+
+  def new
+  end
+
+  def create
+    #Login authenication
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to users_path
+    else
+       flash[:error] = ["Invalid email or password"]
+       redirect_to new_user_path
+    end
+  end
+
+  def destroy
+      session.clear
+      redirect_to root_url
   end
 
 end
