d8c9532c976a4c6e77933598067faf590d330a60
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-29 21:39:16 -0600

    edit and update functions now work in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,28 +24,33 @@ class UsersController < ApplicationController
 
   def edit
     #edit profile page form
-    #Broken
-      @user = User.find(session[:user_id])                
-      
+      @user = User.find(session[:user_id])
+      if request.xhr?
+          render :edit, layout: false
+      end                
   end
 
-  def show
-    #profile page (things to teach and learn)
-     @user = current_user
-  end
 
   def update
     #update profile
-    #Broken
     @user = User.find(session[:user_id])
+    puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    puts @user
+     puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
     if @user.update_attributes(params[:user])
+        puts "****************************************"
         flash[:success] = "Your info has been updated!"
         redirect_to users_path(@user)
     else
+        flash[:success] = "Your info has had a problem updating. Try again"
         render :edit
     end
   end
 
+  def show
+    #profile page (things to teach and learn)
+     #@user = current_user
+  end
   def destroy
   end
 
