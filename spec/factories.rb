4192372ed8098c6e80c53c2d65e6587194b9b745
connor McCrone <your.email@example.com>
2013-11-29 21:18:40 -0600

    added factories file to spec folder with fake data to use for future testing. Added factory girl test data for each migration .

---
+++ spec/factories.rb
@@ -0,0 +1,45 @@
+require 'factory_girl_rails'
+
+FactoryGirl.define do 
+	factory :user do 
+		first_name "Clark"
+		last_name "Kent"
+		email "superman@mail.com"
+		password_digest "password"
+	end
+
+	factory :rating do 
+		score 1 
+		review "You're the bee's knees"
+		meetup_id 1
+		learner_id 1 
+	end
+
+	factory :meetup do 
+		offering_id 1
+		learner_id 1
+		method "google hangout"
+		datetime Time.now
+	end
+
+	factory :offering do 
+		subject_id 1 
+		teacher_id 1
+		hangout true 
+		location "krypton"
+	end
+
+	factory :subject do 
+		name "Ruby"
+		category_id 1
+	end
+
+	factory :categories do 
+		name "programming languages" 
+	end
+end
+
+
+
+
+
