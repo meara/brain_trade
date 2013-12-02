d1688f88ab8834974ff7f025e0ad225284eca4af
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-01 18:38:36 -0600

    learner request accepted or rejected emails

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,5 +2,5 @@ class Meetup < ActiveRecord::Base
   belongs_to :learner, class_name: "User"
   belongs_to :offering
 
-  attr_accessible :method, :learner_id, :offering_id, :date_time, :time1, :time2, :time3
+  attr_accessible :method, :learner_id, :offering_id, :date_time, :time1, :time2, :time3, :accepted
 end
