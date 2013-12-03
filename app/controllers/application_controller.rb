37bbe20c9c87221c9955767a7205f088320d408b
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-03 15:30:00 -0600

    require login to create or update things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :require_login, only: [:create, :update]
+ 
   private
+ 
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_user_path # halts request cycle
+    end
+  end
 
   def current_user
     current_user = User.find_by_id(session[:user_id])
   end
-
-
-  #rails guides version
-  # def current_user
-  #   @_current_user ||= session[:current_user_id] &&
-  #     User.find_by(id: session[:current_user_id])
-  # end
 
 end
