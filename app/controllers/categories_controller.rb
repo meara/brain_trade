84920b3acf0532fbe3068717e3044869ff965e1c
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-04 13:16:52 -0600

    added search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,19 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
+    @categories = Category.search(params[:search])
+    # @categories = Category.all
   end
 
   def show
     @category = Category.find_by_id(params[:id])
-    @subjects = @category.subjects.all
-  
+
+    if Subject.search(params[:search]).present?
+      #@subjects = Subject.search(params[:search])#.find_by(category_id: @category.id)
+      @subjects = Subject.search(params[:search]).where(:category_id => @category.id)
+    else
+      @subjects = @category.subjects.all
+    end
   end
 
 end
